2bb0a4a58f876b19246afda03b36e8aacfd2ba6f
Chaya Phuwichit <chaya@odds.team>
2023-11-02 22:02:23 +0700

    fix: import check form k6

--- k6script/app-lb.js
+++ k6script/app-lb.js
@@ -1,5 +1,8 @@
-import http from 'k6/http';
-
+import http from "k6/http";
+import { check, group, sleep } from "k6";
 export default function () {
-  http.get('http://app-loadbalancer/mock100');
-};
+  let res = http.get("http://app-loadbalancer/mock100");
+  check(res, {
+    "is status 200": (r) => r.status === 200,
+  });
+}
--- k6script/srv01.js
+++ k6script/srv01.js
@@ -1,5 +1,8 @@
 import http from 'k6/http';
-
+import { check, group, sleep } from "k6";
 export default function () {
-  http.get('http://app-srv01:3000/mock100');
+  let res = http.get('http://app-srv01:3000/mock100');
+  check(res, {
+    "is status 200": (r) => r.status === 200,
+  });
 };
